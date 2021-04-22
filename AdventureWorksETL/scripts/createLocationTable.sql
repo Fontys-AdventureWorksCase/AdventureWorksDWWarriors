515697d5eb5ff034fe6e9ac2607c5fdb4288c9cc
Teun Cortooms <teuncortooms@gmail.com>
2021-04-22 21:33:35 +0200

    add scripts for table creation

---
+++ AdventureWorksETL/scripts/createLocationTable.sql
@@ -0,0 +1,20 @@
+IF OBJECT_ID(N'dbo.Dim_Location', N'U') IS NULL 
+BEGIN   
+
+CREATE TABLE [dbo].[Dim_Location](
+	[LocationKey] [int] IDENTITY(1,1) NOT NULL,
+	[City] [nvarchar](30) NOT NULL,
+	[StateProvinceCode] [nvarchar](3) NOT NULL,
+	[StateProvinceName] [nvarchar](50) NOT NULL,
+	[CountryRegionCode] [nvarchar](3) NOT NULL,
+	[CountryRegionName] [nvarchar](50) NOT NULL,
+PRIMARY KEY CLUSTERED 
+([LocationKey] ASC) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, 
+ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) 
+ON [PRIMARY]
+) ON [PRIMARY]
+
+
+
+END;
+GO
