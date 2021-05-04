9990a8f72c02419852dcf85b1bd49f2343ff0338
Teun Cortooms <teuncortooms@gmail.com>
2021-05-04 11:17:00 +0200

    Adapt creation script and fact table flow

--- AdventureWorksETL/scripts/createSalesFactTable.sql
+++ AdventureWorksETL/scripts/createSalesFactTable.sql
@@ -3,22 +3,22 @@ BEGIN
 
 
 CREATE TABLE [dbo].[Fact_Sales] (
-	[SalesOrderID] [int] NOT NULL,
-	[SalesOrderDetailID] [int] NOT NULL,
 	[ProductKey] [int] NOT NULL FOREIGN KEY REFERENCES Dim_Product(ProductKey),
 	[SalesPersonKey] [int] NOT NULL FOREIGN KEY REFERENCES Dim_SalesPerson(SalesPersonKey),
-	[Customer] [int] NOT NULL FOREIGN KEY REFERENCES Dim_Customer(CustomerID),
-	[SalesTerritory] [int] NOT NULL FOREIGN KEY REFERENCES Dim_SalesTerritory(TerritoryKey),
+	[CustomerKey] [int] NOT NULL FOREIGN KEY REFERENCES Dim_Customer(CustomerID),
+	[SalesTerritoryKey] [int] NOT NULL FOREIGN KEY REFERENCES Dim_SalesTerritory(TerritoryKey),
 	[BillToLocationKey] [int] NOT NULL FOREIGN KEY REFERENCES Dim_Location(LocationKey),
 	[ShipToLocationKey] [int] NOT NULL FOREIGN KEY REFERENCES Dim_Location(LocationKey),
 	[OrderDateKey] [int] NOT NULL FOREIGN KEY REFERENCES Dim_Date(DateKey),
 	[DueDateKey] [int] NOT NULL FOREIGN KEY REFERENCES Dim_Date(DateKey),
 	[ShipDateKey] [int] NOT NULL FOREIGN KEY REFERENCES Dim_Date(DateKey),
 	[SalesTypeKey] [int] NOT NULL FOREIGN KEY REFERENCES Dim_SalesType(SalesTypeKey),
+	[SpecialOfferKey] [int] NOT NULL FOREIGN KEY REFERENCES Dim_SpecialOffer(SpecialOfferKey),
 	[OrderQty] [smallint] NULL,
 	[UnitPrice] [money] NULL,
 	[UnitPriceDiscount] [float] NULL,
-	[ProductCost] [money] NULL
+	[ProductCost] [money] NULL,
+	[OrderTotalPrice] [money] NULL
 );
 
 
