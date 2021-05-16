0170dd13a665e1eba2402d5c4cd0f43221ac3070
Richard de Jong <rdejong81@gmail.com>
2021-05-16 17:54:18 +0200

    More performant update from staging table to datawarehouse tables (feedback from Gleb).

--- AdventureWorksETL/scripts/dimSalesTerritoryUpdate.sql
+++ AdventureWorksETL/scripts/dimSalesTerritoryUpdate.sql
@@ -1,38 +1,11 @@
-declare @key int
-declare @territory VARCHAR(50)
-declare @country varchar(50)
-declare @continent varchar(50)
+Update dw
+SET
+dw.Territory = st.Territory, dw.Country = st.Country, dw.Continent = st.Continent
+FROM AdventureWorksDW.dbo.Dim_SalesTerritory dw
+LEFT JOIN StagingDW.dbo.Dim_SalesTerritory_stage_updates st on dw.TerritoryKey = dw.TerritoryKey 
 
-DECLARE updatecur CURSOR FOR 
-SELECT st.TerritoryKey,st.Territory,st.Country,st.Continent
-FROM Dim_SalesTerritory_stage_updates st
-left join AdventureWorksDW.dbo.Dim_SalesTerritory dw on dw.TerritoryKey = st.TerritoryKey
-WHERE 
- dw.Continent <> st.Continent
+Where dw.TerritoryKey = st.TerritoryKey
+and (
+dw.Continent <> st.Continent
 or  dw.Country <> st.Country
-or  dw.Territory <> st.Territory
-
-
-OPEN updatecur
-
-FETCH NEXT FROM updatecur INTO @key,@territory,@country,@continent; 
-
--- Set the status for the cursor
-WHILE @@FETCH_STATUS = 0  
- 
-BEGIN  
-	-- 4 - Begin the custom business logic
-	-- * UPDATE WITH YOUR SPECIFIC CODE HERE *
-   	Update AdventureWorksDW.dbo.Dim_SalesTerritory 
-	set Territory = @territory, Country = @country, Continent = @continent
-	where TerritoryKey = @key;
-
-	-- 5 - Fetch the next record from the cursor
- 	FETCH NEXT FROM updatecur INTO @key,@territory,@country,@continent; 
-END 
-
--- 6 - Close the cursor
-CLOSE updatecur  
-
--- 7 - Deallocate the cursor
-DEALLOCATE updatecur 
+or  dw.Territory <> st.Territory)
